77d06f8dd24252dc5a45294f9969d7c0299f8777
luhaotian <luhaotian@bytedance.com>
2026-01-13 01:17:25 +0800

    fix configs

--- train_examples/train_v1.sh
+++ train_examples/train_v1.sh
@@ -2,7 +2,7 @@ export MODEL_NAME="/mnt/bn/douyin-ai4se-general-wl/lht/ckpt/Wan2.1-Fun-V1.1-1.3B
 export DATASET_NAME=""
 export DATASET_META_NAME="/mnt/bn/douyin-ai4se-general-wl/lht/data/how2sign/train/videx_fun/metadata.json"
 
-NCCL_DEBUG=INFO accelerate launch --mixed_precision="bf16" scripts/wan2.1_fun/train_control.py \
+NCCL_DEBUG=INFO accelerate launch --use_deepspeed --deepspeed_config_file config/zero_stage3_config.json scripts/wan2.1_fun/train_control.py \
   --config_path="config/wan2.1/wan_civitai.yaml" \
   --pretrained_model_name_or_path="$MODEL_NAME" \
   --train_data_dir="$DATASET_NAME" \
@@ -12,13 +12,13 @@ NCCL_DEBUG=INFO accelerate launch --mixed_precision="bf16" scripts/wan2.1_fun/tr
   --token_sample_size=512 \
   --video_sample_stride=2 \
   --video_sample_n_frames=81 \
-  --train_batch_size=1 \
+  --train_batch_size=8 \
   --video_repeat=0 \
   --gradient_accumulation_steps=1 \
   --dataloader_num_workers=8 \
   --num_train_epochs=10 \
-  --checkpointing_steps=50 \
-  --learning_rate=2e-05 \
+  --checkpointing_steps=35 \
+  --learning_rate=5e-06 \
   --lr_scheduler="constant_with_warmup" \
   --lr_warmup_steps=100 \
   --seed=42 \
@@ -27,7 +27,7 @@ NCCL_DEBUG=INFO accelerate launch --mixed_precision="bf16" scripts/wan2.1_fun/tr
   --mixed_precision="bf16" \
   --adam_weight_decay=3e-2 \
   --adam_epsilon=1e-10 \
-  --vae_mini_batch=1 \
+  --vae_mini_batch=8 \
   --max_grad_norm=0.05 \
   --random_hw_adapt \
   --training_with_video_token_length \
@@ -36,5 +36,4 @@ NCCL_DEBUG=INFO accelerate launch --mixed_precision="bf16" scripts/wan2.1_fun/tr
   --low_vram \
   --train_mode="control_ref" \
   --control_ref_image="first_frame" \
-  --add_full_ref_image_in_self_attention \
   --trainable_modules "."
